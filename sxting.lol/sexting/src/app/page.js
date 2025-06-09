63f3c21938423043b9d9bccb5a3bcc5dab00bc35
gaurav2425 <61289234+gaurav2425@users.noreply.github.com>
2025-06-09 09:26:46 +0530

    Sxting

---
+++ sxting.lol/sexting/src/app/page.js
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      <Head>
+        <title>Sexting</title>
+        <meta name="description" content="Sex chat" />
+        <meta
+          name="keywords"
+          content="Sexting, Sex chat, Adult Chat, Sexy chat"
+        />
+        <meta name="author" content="Sexting.lol" />
+      </Head>
+      <main className={styles.main}>
+        <ol>
+          <h1>Comming Soon!</h1>
+        </ol>
+      </main>
+    </div>
+  );
+}
